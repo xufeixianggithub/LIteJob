4bdc03729d39a73f1f4a6d486686481ebc66e628
zude <zude@seeapp.com>
2018-08-06 17:29:44 +0800

    Revert "123"
    This reverts commit 9ac07fd

--- src/main/java/com/alen/Test/Test.java
+++ src/main/java/com/alen/Test/Test.java
@@ -12,7 +12,7 @@ public class Test {
 
 
             }catch (Exception e){
-                System.out.println("捕获到异常了啊123456");
+                System.out.println("捕获到异常了啊1");
                 e.printStackTrace();
             }
         }
